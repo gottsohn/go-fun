7badc94a8b88abb8409392d7c046df70543b0e4f
Godson Ukpere <godson.ukpere@daimler.com>
2018-11-29 15:05:30 +0100

    chore(App): Create base app for go-api

---
+++ config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+// Config Represents database server and credentials
+type Config struct {
+	Server   string
+	Database string
+	Port     int
+}
+
+// Read and parse the configuration file
+func (c *Config) Read() {
+	if _, err := toml.DecodeFile("config.toml", &c); err != nil {
+		log.Fatal(err)
+	}
+}
